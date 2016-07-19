f8e8601c3f71bb6d170dc748449e2fdcda33aff3
rittersm <rittersm@gmail.com>
2016-07-19 19:31:16 -0400

    user repos page added

--- app/controllers/rubyists_controller.rb
+++ app/controllers/rubyists_controller.rb
@@ -1,7 +1,15 @@
 class RubyistsController < ApplicationController
 
   def index
+    rubyist = []
     client = Octokit::Client.new(:access_token => ENV["octokit_auth"])
-    @rubyists = client.search_users("location:Indianapolis language:Ruby", page: params[:id], sort: "joined", order: "asc")[:items]
+    rubyists = client.search_users("location:Indianapolis language:Ruby", page: params[:page], sort: "joined", order: "asc")
+    @indy_ruby = rubyists[:items].map{|x| Octokit.user(x.login)}
+    @pages = (rubyists[:total_count] / 30.0).ceil
+  end
+
+  def show
+    client = Octokit::Client.new(:access_token => ENV["octokit_auth"])
+    @gits = client.list_repos("#{params[:id]}")
   end
 end
