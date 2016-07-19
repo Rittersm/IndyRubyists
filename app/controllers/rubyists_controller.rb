5e24cfa5d67f1f4fb07e39dec48fed1cd3d0bbb0
rittersm <rittersm@gmail.com>
2016-07-19 15:27:22 -0400

    functional list of rubyists

--- app/controllers/rubyists_controller.rb
+++ app/controllers/rubyists_controller.rb
@@ -1,2 +1,7 @@
 class RubyistsController < ApplicationController
+
+  def index
+    client = Octokit::Client.new(:access_token => ENV["octokit_auth"])
+    @rubyists = client.search_users("location:Indianapolis language:Ruby", page: params[:id], sort: "joined", order: "asc")[:items]
+  end
 end
